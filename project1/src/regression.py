ccefb405a373d6fc30e4c7b51c66895205abdff5
Geir Ulvik <ulvik@usit.uio.no>
2018-09-27 15:55:43 +0200

    Modified plotting to give side-by-side plots for comparison.

--- project1/src/regression.py
+++ project1/src/regression.py
@@ -34,19 +34,31 @@ beta = np.linalg.inv(X_vals.T.dot(X_vals)).dot(X_vals.T).dot(z)
 z_approx = X_vals.dot(beta)
 X_plot, Y_plot = np.meshgrid(X[:,0], X[:,1])
 
-
+# =================================================================
 # Plotting
+# Plots the least squares fit and the FrankeFunction next to
+# each other for comparison.
 fig = plt.figure()
-ax = fig.gca(projection='3d')
-surf = ax.plot_surface(X_plot, Y_plot, z_approx, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
+ax = fig.add_subplot(1, 2, 1, projection='3d')
 
+surf1 = ax.plot_surface(X_plot, Y_plot, z_approx, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
 # Customize the z axis.
 ax.set_zlim(-0.10, 1.40)
 ax.zaxis.set_major_locator(LinearLocator(10))
 ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
 
 # Add a color bar which maps values to colors.
-fig.colorbar(surf, shrink=0.5, aspect=5)
+fig.colorbar(surf1, shrink=0.5, aspect=5)
+
+# Second subplot
+ax = fig.add_subplot(1,2,2, projection='3d')
+surf2 = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+ax.set_zlim(-0.10, 1.40)
+ax.zaxis.set_major_locator(LinearLocator(10))
+ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+fig.colorbar(surf2, shrink=0.5, aspect=5)
 
 plt.show()
