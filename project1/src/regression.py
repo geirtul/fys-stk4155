f92033ad59bb935437163f7a4350e18356ec7355
Idun Kløvstad <idunklovstad@1x-193-157-204-24.uio.no>
2018-09-28 16:18:54 +0200

    merging?

--- project1/src/regression.py
+++ project1/src/regression.py
@@ -9,33 +9,31 @@ from mpl_toolkits.mplot3d import Axes3D
 
 #making x and y
 X = np.random.uniform(0,1, (20, 2))
-print (X.shape)
+X = np.sort(X, axis=0) #sort x values
 
 x = X[:,0]
 y = X[:,1]
 
-x, y = np.meshgrid(x, y)
+x_plot, y_plot = np.meshgrid(x, y)
 
 #setting up a fifth order polynomial and setting in for x an y. 
 #Also addes a bias line of ones. 
 poly = PolynomialFeatures(5)
 fifthPoly = poly.fit_transform(X) 
-# print (poly.get_feature_names())
-print (fifthPoly.shape)
+print (poly.get_feature_names())
 
 z = FrankeFunction(x, y)
 
-beta = np.linalg.inv( X.T @ X ) @ X.T @ z 
-
-print (beta.shape) 
+beta = np.linalg.inv( fifthPoly.T @ fifthPoly ) @ fifthPoly.T @ z
+z_approx = fifthPoly @ beta 
 
 
 #plot franke_function
+
 fig = plt.figure()
-ax = fig.gca(projection='3d')
+ax = fig.add_subplot(1,2,1, projection='3d')
 
-
-surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
+surf1 = ax.plot_surface(x_plot, y_plot, z_approx, cmap=cm.coolwarm,
                        linewidth=0, antialiased=False)
 
 # Customize the z axis.
@@ -44,12 +42,21 @@ ax.zaxis.set_major_locator(LinearLocator(10))
 ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
 
 # Add a color bar which maps values to colors.
-fig.colorbar(surf, shrink=0.5, aspect=5)
+fig.colorbar(surf1, shrink=0.5, aspect=5)
+
+
+# Second subplot
+ax = fig.add_subplot(1,2,2, projection='3d')
+surf2 = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+ax.set_zlim(-0.10, 1.40)
+ax.zaxis.set_major_locator(LinearLocator(10))
+ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+fig.colorbar(surf2, shrink=0.5, aspect=5)
 
 plt.show()
-
-
-
 
 
 
