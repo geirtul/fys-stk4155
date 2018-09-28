fd2635cb753eecdebb29ea45bf12f61b752762f5
Geir Ulvik <ulvik@usit.uio.no>
2018-09-28 14:58:30 +0200

    Cleaned up the regression code and added MSE and R2 score evaluation (latter two not tested.)

--- project1/src/regression.py
+++ project1/src/regression.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 from mpl_toolkits.mplot3d import Axes3D
 
-
 # Data from FrankeFunction
 x = np.arange(0, 1, 0.05)
 y = np.arange(0, 1, 0.05)
@@ -22,17 +21,65 @@ def FrankeFunction(x,y):
 
 z = FrankeFunction(x, y)
 
+# Function definitions
+def mean_squared_error(y, y_predicted):
+    """
+    Evaluate the mean squared error of the output generated
+    by Ordinary Least Squares regressions.
+    y : the data on which OLS was performed on
+    y_predicted : the data the model spits out after regression
+    """
+
+    N = y.shape[0]
+    mse = np.sum(np.square(y - y_predicted))/N
+    return mse
+
+def r2_score(y, y_predicted):
+    """
+    Evaluate the R2 score function.
+    y : the data on which OLS was performed on
+    y_predicted : the data the model spits out after regression
+    y_mean : mean value of y
+    upper/lower_sum : numerator/denominator in R2 definition.
+    """
+    N = y.shape[0]
+    y_mean = np.sum(y)/N
+    upper_sum = np.sum(np.square(y - y_predicted))
+    lower_sum = np.sum(np.square(y - y_mean))
+    r2score = 1 - upper_sum/lower_sum
+
+
 # Setting up the matrix X in the matrix equation y = X*Beta + Eps
+# and perform regression
 
-X = np.random.uniform(0, 1, (20,2))
-X = np.sort(X, axis=0) # Sort the x-values
-poly = PolynomialFeatures(5)
-X_vals = poly.fit_transform(X)
+def ordinary_least_squares(y, m):
+    """
+    Perform linear regression using th ordinary least squares method
+    on a dataset y, with a polynomial of degree m.
+    The PolynomialFeatures module from scikit learn sets up the vandermonde
+    matrix such that in the matrix equation X*beta = y, beta is the coefficient vector,
+    and X contains the polynomial expressions.
+    returns x and y values for plotting along with the predicted y values
+    from the model.
+    """
 
-beta = np.linalg.inv(X_vals.T.dot(X_vals)).dot(X_vals.T).dot(z)
+    # Setup
+    N = y.shape[0]
+    X_vals = np.random.uniform(0, 1, (N,2))
+    X_vals = np.sort(X_vals, axis=0) # Sort the x-values
+    poly = PolynomialFeatures(m)
 
-z_approx = X_vals.dot(beta)
-X_plot, Y_plot = np.meshgrid(X[:,0], X[:,1])
+    # Regression
+    X = poly.fit_transform(X_vals)
+    beta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
+    y_predicted = X.dot(beta)
+
+    # Output
+    X_plot, Y_plot = np.meshgrid(X_vals[:,0], X_vals[:,1])
+    return [X_plot, Y_plot, y_predicted]
+
+
+output = ordinary_least_squares(z,5)
 
 # =================================================================
 # Plotting
@@ -41,7 +88,7 @@ X_plot, Y_plot = np.meshgrid(X[:,0], X[:,1])
 fig = plt.figure()
 ax = fig.add_subplot(1, 2, 1, projection='3d')
 
-surf1 = ax.plot_surface(X_plot, Y_plot, z_approx, cmap=cm.coolwarm,
+surf1 = ax.plot_surface(output[0], output[1], output[2], cmap=cm.coolwarm,
                        linewidth=0, antialiased=False)
 # Customize the z axis.
 ax.set_zlim(-0.10, 1.40)
