bd3e432436fa74cb3acb4d0263820453bec499e0
geirtul <g.t.ulvik@fys.uio.no>
2018-10-23 19:55:01 +0200

    Uploading more updated code.

--- project2/src/analysis.py
+++ project2/src/analysis.py
@@ -1,111 +1,117 @@
 import numpy as np
-from time import time
-from numpy.random import randint
-from sklearn.preprocessing import PolynomialFeatures
+from sklearn.model_selection import train_test_split
+from sklearn.utils import resample
 from matplotlib import cm
-from random import random, seed
 import matplotlib.pyplot as plt
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 from mpl_toolkits.mplot3d import Axes3D
 
+
 class Analysis:
 
     def mean_squared_error(self):
-
-        """Evaluate the mean squared error of the output generated
+        """
+        Evaluate the mean squared error of the output generated
         by Ordinary Least Squares regressions.
 
-        y               - the data on which OLS was performed on
-        y_predicted     - the data the model spits out after regression
+        outcome               - the data on which regression was performed on
+        predicted_outcome     - the data the model spits out after regression
         """
+        # TODO: This method only works when bootstrap is not being performed.
+        # TODO: Concider generalizing so that this method can be called inside
+        # TODO: bootstrap?
 
-        num_datapoints = self.z.shape[0]
-        mse = np.sum(np.square(self.z - self.z_predicted))/num_datapoints
+        N = self.outcome.size
+        mse = np.sum(np.square(self.outcome - self.predicted_outcome)) / N
         return mse
 
     def r2_score(self):
+        """
+        Evaluate the R2 score of the model fitted to the dataset.
 
-        """ Evaluate the R2 score function.
-
-        y               - the data on which OLS was performed on
-        y_predicted     - the data the model spits out after regression
-        y_mean          -  mean value of y
-        upper/lower_sum - numerator/denominator in R2 definition.
+        :return:    Returns the mean squared error of the model compared with
+                    dataset used for fitting.
         """
 
-        N = self.z.shape[0]
-        print(self.z_predicted)
-        z_mean = np.sum(self.z)/N
-        upper_sum = np.sum(np.square(self.z - self.z_predicted))
-        lower_sum = np.sum(np.square(self.z - z_mean))
-        r2score = 1 - upper_sum/lower_sum
+        outcome_mean = np.mean(self.outcome)
+        upper_sum = np.sum(np.square(self.outcome - self.predicted_outcome))
+        lower_sum = np.sum(np.square(self.outcome - outcome_mean))
+        r2score = 1 - upper_sum / lower_sum
         return r2score
 
     def bootstrap(self):
         """
-        Perform the chosen regression using bootstrapping.
+        Perform the chosen regression using bootstrap algorithm.
         """
-        N = len(self.z)
+
+        def statistics(testdata, predicted_data):
+            """
+            Calculates useful statistics.
+
+            :param testdata: Data to compare the fitted data with
+            :param predicted_data: Data predicted by the model
+            :return: mean square error, bias, variance
+            """
+            mse = np.mean(np.mean(np.square(testdata - predicted_data)))
+            bias = np.mean(np.square(testdata - np.mean(predicted_data)))
+            variance = np.mean(np.var(predicted_data))
+            return mse, bias, variance
+
         n_bootstraps = 100
 
         # Split into training and test sets
-        test_size = 0.2
-        stop = int(np.floor(test_size*N))
-        x_test = self.X_vals[0:stop, :]
-        data_test = self.z[0:stop, :]
-        #x_train = self.X_vals[test_size*N:,:]
-        data_train = self.z[stop:,:]
+        x_train, x_test, data_train, data_test = train_test_split(self.predictors,
+                                                                  self.outcome,
+                                                                  test_size=0.2)
 
-        def resample(data):
-            points = len(data)
-            #x_indices = randint(0, points, x.shape)
-            data_indices = randint(0,points,data.shape)
-            return data[data_indices]
-
-        
-        y_fits = np.empty((len(data_train), n_bootstraps))
+        y_fits = np.empty((n_bootstraps, len(data_test.ravel())))
         for i in range(n_bootstraps):
-            
-            data_re = resample(data_train)
-            self.fitCoefficients(5, 2, data_re)
-            y_fits[i] = self.makePrediction()
+            x_re, data_re = resample(x_train, data_train, replace=True)
+            self.fit_coefficients(x_re, data_re, self.poly_degree)
+            newfit = self.make_prediction(x_test, data_test)
+            y_fits[i] = newfit.ravel()
 
-        
-        
-        print("Mean y_fit = {}".format(np.mean(y_fits)))
-        """
-        #Some pretty printing we probably don't need.
+        mse, bias, variance = statistics(data_test, y_fits)
+
+        # Output
         print("Bootstrap statistics:")
-        print("{:^8s} | {:^8s} | {:^8s} | {:^8s}".format("original", "bias", "mean", "std.err"))
-        print("{:8f} | {:8f} | {:8f} | {:8f}".format( np.mean(self.z),
-                                                np.std(self.z),
-                                                np.mean(t),
-                                                np.std(t)))
-        """
+        print("{:^8s} | {:^8s} | {:^8s} | {:^8s}".format("original",
+                                                         "bias",
+                                                         "mean_fit",
+                                                         "std.err"))
+        print("{:8f} | {:8f} | {:8f} | {:8f}".format(np.mean(self.outcome),
+                                                     np.std(self.outcome),
+                                                     np.mean(y_fits),
+                                                     np.std(y_fits)))
+        print("MSE = ", mse)
+        print("Bias = ", bias)
+        print("Var = ", variance)
+        print("{} >= {} + {} = {}".format(mse, bias, variance, bias+variance))
+
         return y_fits
 
-
-    def plotting_3d(self, data, output, save=False):
-
-        """ Plots the modeled data side-by-side with the original dataset
+    def plotting_3d(self, save=False, extension=""):
+        """
+        Plots the modeled data side-by-side with the original dataset
         for comparison.
-
-        data    - list[x, y, z]
-                  contains x, y meshgrid and solution from FrankeFunction.
-
-        output  - list[x, y, z_predicted]
-                  contains x, y meshgrid and predicted solution.
         """
 
-        self.data = data
-        self.output = output
+        # Doing some reshaping business for plotting in 3D.
+        x, y = self.predictors[:,0], self.predictors[:,1]
+        z, z_predict = self.outcome, self.predicted_outcome
+        N = int(np.sqrt(x.size))
+        x = x.reshape((N,N))
+        y = y.reshape((N,N))
+        z = z.reshape((N,N))
+        z_predict = z_predict.reshape((N,N))
 
         fig = plt.figure()
         ax = fig.add_subplot(1, 2, 1, projection='3d')
 
-        surf1 = ax.plot_surface(self.output[0], self.output[1],
-                                self.output[2], cmap=cm.coolwarm,
-                                linewidth=0, antialiased=False)
+        surf1 = ax.plot_surface(x, y, z_predict,
+                                cmap=cm.coolwarm,
+                                linewidth=0,
+                                antialiased=False)
         # Customize the z axis.
         ax.set_zlim(-0.10, 1.40)
         ax.zaxis.set_major_locator(LinearLocator(10))
@@ -115,10 +121,11 @@ class Analysis:
         fig.colorbar(surf1, shrink=0.5, aspect=5)
 
         # Second subplot
-        ax = fig.add_subplot(1,2,2, projection='3d')
-        surf2 = ax.plot_surface(self.data[0], self.data[1],
-                                self.data[2], cmap=cm.coolwarm,
-                                linewidth=0, antialiased=False)
+        ax = fig.add_subplot(1, 2, 2, projection='3d')
+        surf2 = ax.plot_surface(x, y, z,
+                                cmap=cm.coolwarm,
+                                linewidth=0,
+                                antialiased=False)
         ax.set_zlim(-0.10, 1.40)
         ax.zaxis.set_major_locator(LinearLocator(10))
         ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
@@ -126,5 +133,10 @@ class Analysis:
         fig.colorbar(surf2, shrink=0.5, aspect=5)
 
         if save:
-            plt.savefig("../report/figures/comparison.pdf", format='pdf')
+            plt.savefig("../report/figures/comparison_{}.pdf".format(extension),
+                        format='pdf')
         plt.show()
+
+
+if __name__ == "__main__":
+    print("Loaded Analysis.py")
---
+++ project2/src/lasso.py
@@ -0,0 +1,111 @@
+import numpy as np
+from franke_function import FrankeFunction 
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import linear_model
+from analysis import Analysis
+from sklearn.model_selection import train_test_split
+
+
+class LassoRegression(Analysis):
+
+    def __init__(self):
+        """
+        Perform regression using the Lasso method
+        on a dataset y, with a polynomial of degree poly_degree.
+        The PolynomialFeatures module from scikit learn sets up the 
+        vandermonde matrix such that in the matrix equation X*beta = y, 
+        beta is the coefficient vector, and X contains the polynomial
+        expressions.
+
+        The method uses scikit learn's Lasso regression methods.
+        """
+
+        self.predictors = None
+        self.poly_degree = None
+        self.outcome = None
+        self.beta = None
+        self.poly = None
+        self.predicted_outcome = None
+        self.alpha = None
+        self.lasso_object = None
+
+    def fit_coefficients(self, predictors, outcome, poly_degree, alpha=1.0):
+        """
+        Fits the polynomial coefficients beta to the matrix
+        of polynomial features.
+
+        :param predictors: x,y, ... values that generated the outcome
+        :param outcome: the dataset we will fit
+        :param poly_degree: Degree of the polynomial to fit
+        :param alpha: float, Constant that multiplies the L1 term.
+                        Defaults to 1.0. alpha = 0 is equivalent to
+                        an ordinary least square regression.
+        """
+
+        self.poly_degree = poly_degree
+        self.poly = PolynomialFeatures(poly_degree)
+        self.outcome = outcome
+        self.alpha = alpha
+
+        # Regression
+        # Input values to design matrix
+        self.predictors = self.poly.fit_transform(predictors)
+        
+        self.lasso_object = linear_model.Lasso(alpha=self.alpha, max_iter=1e3)
+        self.lasso_object.fit(self.predictors, self.outcome)
+
+    def make_prediction(self, x_in, z_in):
+        """
+        Makes a model prediction.
+        Demands both x and z- values in order to update the target values
+        (self.outcome) for MSE calculations. This can probably be done better.
+        """
+        X = self.poly.fit_transform(x_in)
+        self.predicted_outcome = self.lasso_object.predict(X)
+        self.outcome = z_in
+
+        return self.predicted_outcome
+
+
+if __name__ == "__main__":
+    # Data from FrankeFunction
+    x = np.arange(0, 1, 0.05)
+    y = np.arange(0, 1, 0.05)
+    x, y = np.meshgrid(x, y)
+    z = FrankeFunction(x, y)
+
+    # Make predictor values and data a matrix with
+    # number of columns = number of predictors.
+    predictors_input = np.c_[x.ravel(), y.ravel()]
+    z = z.ravel()
+
+    lasso = LassoRegression()
+    lasso.fit_coefficients(predictors_input, z, 5, alpha=0.001)
+    lasso.make_prediction(predictors_input, z)
+    lasso.bootstrap()
+
+    """
+    x_train, x_test, data_train, data_test = train_test_split(predictors_input,
+                                                              z,
+                                                              test_size=0.2)
+
+    alpha_vals = [1e-5, 1e-4, 1e-3, 1e-2]
+
+    for alpha in alpha_vals:
+        print("Running Lasso Regression with alpha = {}\n".format(alpha))
+        lasso = LassoRegression()
+        lasso.fit_coefficients(x_train, data_train, 5, alpha=alpha)
+        lasso.make_prediction(x_test, data_test)
+
+        print("MSE = ", lasso.mean_squared_error())
+        print("R2: ", lasso.r2_score())
+        print("===================================\n")
+
+    noiseRange = 10
+    noise = noiseRange*np.random.uniform(-0.5, 0.5, size = z.shape)
+    z  = z - z.mean(1)[:, np.newaxis]
+    z = z + noise
+    """
+
+
+
