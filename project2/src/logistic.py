d5de86fe141038c548b4798318ae5d8c1a587510
Geir Tore Ulvik <ulvik@luring.uio.no>
2018-11-06 18:55:49 +0100

    Adding datafiles and slight changes to logistic.

--- project2/src/logistic.py
+++ project2/src/logistic.py
@@ -10,30 +10,27 @@ class LogisticRegression(Analysis):
         """
 
         self.x = None
-        self.y = None
-        self.coeff = None
+        self.beta = None
 
-    def fit_coefficients(self, x, y):
+    def fit_coefficients(self, x, beta):
         """
-        make s a fit of probabilities.
+        makes a fit of probabilities.
 
         :param x: x values that generated the outcome
-        :param y: the dataset to fit
+        :param beta: the set of weights to optimize
         """
         self.x = x
-        self.y = y
+        self.beta = beta
 
         # Regression
-        self.coeff = np.linalg.pinv(x.T @ x) @ x.T @ y
 
     def make_prediction(self, x):
         """
-        Use the trained model to predict values given an input x.
 
         :param x: Input values to predict new data for
         :return: predicted values
         """
 
-        y_predict = x @ self.coeff
+        #y_predict = x @ self.coeff
 
         return y_predict
