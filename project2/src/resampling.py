d1963a4431a9164ad3285311edf68f2463399c44
Geir Tore Ulvik <ulvik@usit.uio.no>
2018-11-21 16:43:59 +0100

    Modified task_ab etc to do bootstrapping.

--- project2/src/resampling.py
+++ project2/src/resampling.py
@@ -9,7 +9,7 @@ class Resampling:
     Class containing resampling methods to be used with regression classes.
     """
 
-    def bootstrap(self, num_bootstraps = 1000, test_size = 0.1):
+    def bootstrap(self, num_bootstraps = 100, test_size = 0.1, datasize = 100):
         """
         Note:   This code is heavily inspired by the Piazza post
                 "On Bias and Variance" from project 1.
@@ -20,7 +20,7 @@ class Resampling:
         with whatever parameters necessary (alpha, lmb etc).
 
         :param num_bootstraps:  Number of random draws to make.
-                                Defaults to 1000.
+                                Defaults to 100.
         :param test_size:   Relative size of test_data compared to full data.
                             Defaults to 0.1.
 
@@ -29,9 +29,14 @@ class Resampling:
 
         # Split the data the model was initially fit on into training and test
         # sets. Recommended test_size is around 0.1, so we'll stick to that.
+        indices = np.arange(len(self.y))
+        chosen_indices = np.random.choice(
+                indices, size=datasize, replace=False)
+        data_x = self.x[chosen_indices]
+        data_y = self.y[chosen_indices]
 
-        x_train, x_test, y_train, y_test = train_test_split(self.x,
-                                                            self.y,
+        x_train, x_test, y_train, y_test = train_test_split(data_x,
+                                                            data_y,
                                                             test_size=test_size)
 
         y_pred = np.empty((y_test.shape[0], num_bootstraps))
@@ -51,10 +56,10 @@ class Resampling:
         bias = np.mean((y_test - np.mean(y_pred, axis=1, keepdims=True)) ** 2)
         variance = np.mean(np.var(y_pred, axis=1, keepdims=True))
 
-        print('Error:', error)
-        print('Bias^2:', bias)
-        print('Var:', variance)
-        print(
-            '{} >= {} + {} = {}'.format(error, bias, variance, bias + variance))
+        #print('Error:', error)
+        #print('Bias^2:', bias)
+        #print('Var:', variance)
+        #print(
+        #    '{} >= {} + {} = {}'.format(error, bias, variance, bias + variance))
 
-        return y_pred
+        return [error, bias, variance]
